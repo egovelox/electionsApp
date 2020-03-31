5de3d6d794878c3c01f23422cef9f7aaded25c7e
cunc <cunc@cuncmac.local>
2020-03-31 17:35:10 +0200

    added BureauDAO.selecAll

---
+++ src/main/java/fr/mrichard/elections/dao/AccessBase.java
@@ -0,0 +1,68 @@
+package fr.mrichard.elections.dao;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class AccessBase {
+
+    //Attributs
+    private static Connection connection;
+
+    private AccessBase(){
+        
+    };
+
+    //Constructeur en mode Singleton
+    private AccessBase(String propsFilePath) {
+
+        try {
+            Properties props = new Properties();
+            FileInputStream fileReader = new FileInputStream(propsFilePath);
+            props.load(fileReader);
+            fileReader.close();
+
+            //set props
+            String protocol = props.get("protocol").toString();
+            String address = props.get("address").toString();
+            String port = props.get("port").toString();
+            String database = props.get("database").toString();
+            String driver = props.get("driver").toString();
+
+
+            //url=jdbc:postgresql://127.0.0.1:9897/elections
+            String URL = protocol + "://" + address + ":" + port + "/" + database;
+            String USER = props.get("user").toString();
+            String PWD = props.get("pwd").toString();
+
+            Class.forName(driver);
+            connection = DriverManager.getConnection(URL, USER, PWD);
+            connection.setAutoCommit(false);
+
+        } catch (ClassNotFoundException | IOException | SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public static Connection getInstance(String propsFilePath) {
+
+        if (connection == null) {
+            new AccessBase(propsFilePath);
+            connection = getConnection();
+        }
+
+        return connection;
+    } 
+    
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public static void setConnection(Connection connection) {
+        AccessBase.connection = connection;
+    }
+
+}
--- src/main/java/fr/mrichard/elections/dao/BureauDAO.java
+++ src/main/java/fr/mrichard/elections/dao/BureauDAO.java
@@ -1,4 +1,52 @@
 package fr.mrichard.elections.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import fr.mrichard.elections.entity.Bureau;
+
+
 public class BureauDAO {
+
+    public List<Bureau> selectAll() {
+
+        List<Bureau> list = new ArrayList<Bureau>();
+        PreparedStatement prepSt8;
+        ResultSet resultSet;
+        String query = SELECT_ALL;
+
+        try {
+            prepSt8 = connection.prepareStatement(query);
+            resultSet = prepSt8.executeQuery();
+
+            while (resultSet.next()) {
+                Bureau object = new Bureau();
+                object.setIdbureau(resultSet.getInt("idbureau"));
+                object.setCodebureau(resultSet.getString("codebureau"));
+                object.setAdressebureau(resultSet.getString("adressebureau"));
+                object.setLieubureau(resultSet.getString("lieubureau"));
+                object.setInscrits(resultSet.getInt("inscrits"));
+                object.setVotants(resultSet.getInt("votants"));
+                object.setExprimes(resultSet.getInt("exprimes"));
+                object.setNuls(resultSet.getInt("nuls"));
+                object.setProcurations(resultSet.getInt("procurations"));
+                object.setIdarrondissement(resultSet.getInt("idarrondissement"));
+                list.add(object);
+            } /// while
+
+            resultSet.close();
+            prepSt8.close();
+        } catch (SQLException e) {
+            Bureau object = new Bureau();
+            object.setIdbureau(0);
+            object.setCodebureau(e.getMessage());
+            list.add(object);
+            System.out.println(e.getMessage());
+        }
+
+        return list;
+    }
+
 }
